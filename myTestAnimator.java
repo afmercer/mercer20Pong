cd24fac55a7528aa318e09c8ce4a558df4db57e8
amercer98 <mercer20@up.edu>
2018-03-20 00:27:03 -0700

    all walls and rebounding implemented

--- myTestAnimator.java
+++ myTestAnimator.java
@@ -5,16 +5,24 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.MotionEvent;
 
+import java.util.Random;
+
 /**
  * Created by AdamMercer on 3/19/18.
  */
 
 public class myTestAnimator implements Animator {
 
-    private int xDir = 10;
-    private int yDir = 15;
-    private int xCoord = 100;
-    private int yCoord = 500;
+    private double xDir;
+    private double yDir;
+    private double xCoord;
+    private double yCoord;
+    private boolean moveRight;
+    private boolean moveDown;
+
+    public myTestAnimator() {
+        newBall();
+    }
 
     @Override
     public int interval() {
@@ -36,12 +44,44 @@ public class myTestAnimator implements Animator {
         return false;
     }
 
+    public void newBall() {
+        xCoord = 1024;
+        yCoord = 696;
+
+        Random rand = new Random();
+        int direction = rand.nextInt(4);
+        if(direction == 0) {
+            xDir = 15 + rand.nextInt(6);
+            yDir = 15 + rand.nextInt(6);
+            moveRight = true;
+            moveDown = true;
+        }
+        else if(direction == 1) {
+            xDir = -20 + rand.nextInt(6);
+            yDir = 15 + rand.nextInt(6);
+            moveRight = false;
+            moveDown = true;
+        }
+        else if(direction == 2) {
+            xDir = 15 + rand.nextInt(6);
+            yDir = -20 + rand.nextInt(6);
+            moveRight = true;
+            moveDown = false;
+        }
+        else if(direction == 3) {
+            xDir = -20 + rand.nextInt(6);
+            yDir = -20 + rand.nextInt(6);
+            moveRight = false;
+            moveDown = false;
+        }
+
+    }
+
     @Override
     public void tick(Canvas canvas) {
         //TODO citation
         int width = canvas.getWidth();
         int height = canvas.getHeight();
-
 
         //draw walls of playing area
         Paint whitePaint = new Paint();
@@ -53,20 +93,60 @@ public class myTestAnimator implements Animator {
         canvas.drawRect(0, 600, 15, height-600, whitePaint);
 
 
-
-        int num = xCoord%200;
         double angle = Math.atan(yDir/xDir);
-        if(num < 5) {
-            xDir = (int)(10*Math.cos(angle));
-            yDir = -(int)(10*Math.sin(angle));
+
+        if(xCoord+50 >= width-15 && moveRight) {
+            if(moveDown) {
+                xDir = -(20 * Math.cos(angle));
+                yDir = (20 * Math.sin(angle));
+            }
+            else {
+                xDir = -(20 * Math.cos(angle));
+                yDir = (20 * Math.sin(angle));
+            }
+            moveRight = false;
+        }
+        else if(xCoord-50 < 15 && xCoord-20 > 0 && yCoord > 600 && yCoord < height-600 && !moveRight) {
+            if(moveDown) {
+                xDir = (20 * Math.cos(angle));
+                yDir = -(20 * Math.sin(angle));
+            }
+            else {
+                xDir = (20 * Math.cos(angle));
+                yDir = -(20 * Math.sin(angle));
+            }
+            moveRight = true;
+        }
+        else if(yCoord-50 <= 15 && !moveDown) {
+            if(moveRight) {
+                xDir = (20 * Math.cos(angle));
+                yDir = -(20 * Math.sin(angle));
+            }
+            else {
+                xDir = -(20 * Math.cos(angle));
+                yDir = (20 * Math.sin(angle));
+            }
+            moveDown = true;
+        }
+        else if(yCoord+50 >= height-15 && moveDown) {
+            if(moveRight) {
+                xDir = (20 * Math.cos(angle));
+                yDir = -(20 * Math.sin(angle));
+            }
+            else {
+                xDir = -(20 * Math.cos(angle));
+                yDir = (20 * Math.sin(angle));
+            }
+            moveDown = false;
+        }
+        else if(xCoord+50 < 0) {
+            newBall();
         }
 
         xCoord = xCoord+xDir;
         yCoord = yCoord+yDir;
 
-
-
-        canvas.drawCircle(xCoord, yCoord, 50, whitePaint);
+        canvas.drawCircle((int)xCoord, (int)yCoord, 50, whitePaint);
 
     }
 
